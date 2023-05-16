171460312ce9a40eb218f72332456184be04e335
parthpandyappp <parthpandyappp@gmail.com>
2023-05-17 01:45:13 +0530

    chore: typography

--- src/muitheme.ts
+++ src/muitheme.ts
@@ -12,14 +12,21 @@ const theme = createTheme({
   typography: {
     fontFamily: "Proxima Nova",
     fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
     h1: {
       fontSize: "3rem",
+      fontWeight: 500,
     },
     h2: {
       fontSize: "2.5rem",
+      fontWeight: 500,
     },
     h3: {
       fontSize: "2rem",
+      fontWeight: 500,
     },
   },
   components: {
